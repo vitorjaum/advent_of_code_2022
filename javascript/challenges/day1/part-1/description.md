6f97eaf18cefaf3025a6874b96a0932549da97fd
vitorjaum <vitorjaum711@gmail.com>
2022-12-17 01:27:55 -0400

    Refactory

--- javascript/challenges/day1/part-1/description.md
+++ javascript/challenges/day1/part-1/description.md
@@ -1,4 +1,8 @@
-<h1>--- Day 1: Calorie Counting ---</h1>
+<h1>🌲✨Advent of Code 2022 ✨🌲</h1>
+
+<h2>--- Day 1: Calorie Counting ---</h2>
+
+Link: https://adventofcode.com/2022/day/1
 
 Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on Christmas. For that, their favorite snack is a special type of star fruit that only grows deep in the jungle. The Elves have brought you on their annual expedition to the grove where the fruit grows.
 
--- javascript/challenges/day1/part-1/index.js
+++ javascript/challenges/day1/part-1/index.js
@@ -1,17 +1,21 @@
 async function loadFile(file) {
   let text = await file.text();
 
-  const splitedValues = text.split("\n\n");
-  const arrayOfValues = [];
+  const bagsList = text.split("\n\n");
+  const bagsCalories = [];
 
-  splitedValues.map((arr, idx) => {
+  bagsList.map((arr, idx) => {
     const bag = arr.split("\n");
     const bagValue = bag.reduce(
       (accumulador, value) => accumulador + Number(value),
       0
     );
-    arrayOfValues[idx] = bagValue;
+    bagsCalories[idx] = bagValue;
   });
 
-  document.getElementById("output").innerText = Math.max(...arrayOfValues);
+  document.getElementById("output").innerText = getHighestValue(bagsCalories);
+}
+
+function getHighestValue(value) {
+  return Math.max(...value);
 }
